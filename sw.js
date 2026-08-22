9a4798fb998232d15520081e1b5191680e699235
Shaheen-duas <shaheennamaj92@gmail.com>
2026-08-22 15:24:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,41 @@
-const CACHE_NAME = "roz-ki-dua-v13";
+const CACHE_NAME = "roz-ki-dua-v14";
 const FILES_TO_CACHE = [
   "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
+  "index.html",
+  "app.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  // sirf apni site ki files ke liye cache use karo, bahar ki cheezein
-  // (jaise audio API) hamesha seedha internet se hi jaani chahiye
-  if (event.request.url.indexOf(self.location.origin) !== 0) {
-    return;
-  }
-  // network-first: pehle latest file internet se laane ki koshish karo,
-  // sirf tabhi purani (cached) file dikhao jab internet na ho
   event.respondWith(
     fetch(event.request)
       .then((response) => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
       .catch(() => caches.match(event.request))
